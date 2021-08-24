4073df8732f1487a684cae7ba39e66ca1f913a24
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-08-24 08:04:33 -0300

    separe itens in components

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,4 +1,5 @@
 import { Stack, Button, Box } from "@chakra-ui/react";
+import { PaginationItem } from "./PaginationItem";
 
 export function Pagination() {
     return(
@@ -14,59 +15,11 @@ export function Pagination() {
                 <strong>0</strong> - <strong> 10 </strong> de <strong>100</strong>
             </Box>
             <Stack direction="row" spacing="2">
-
-                <Button
-                size="sm"
-                fontSize="xs"
-                width="4"
-                colorScheme="pink"
-                disabled
-                _disabled={{
-                    bg:"pink.500",
-                    cursor: "default",
-                }}
-                >
-                    1
-                </Button>
-                <Button
-                size="sm"
-                fontSize="xs"
-                width="4"
-                colorScheme="pink"
-                disabled
-                _disabled={{
-                    bg:"gray.500",
-                    cursor: "default",
-                }}
-                >
-                    2
-                </Button>
-                <Button
-                size="sm"
-                fontSize="xs"
-                width="4"
-                colorScheme="pink"
-                disabled
-                _disabled={{
-                    bg:"gray.500",
-                    cursor: "default",
-                }}
-                >
-                    3
-                </Button>
-                <Button
-                size="sm"
-                fontSize="xs"
-                width="4"
-                colorScheme="pink"
-                disabled
-                _disabled={{
-                    bg:'gray.500',
-                    cursor: 'default',
-                }}
-                >
-                    4
-                </Button>
+            <PaginationItem number={1} isCurrent/>
+            <PaginationItem number={2} />
+            <PaginationItem number={3} />
+            <PaginationItem number={4} />
+            <PaginationItem number={5} />
             </Stack>
         </Stack>
     )
