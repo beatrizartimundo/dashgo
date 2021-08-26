81784c58a1e4f5d5f56758da0ad1715bafd86fa3
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-08-26 08:05:10 -0300

    finish responsive class

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,72 +1,83 @@
-import { Box, Flex , Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
 import { Sidebar } from "../../components/Sidebar";
 import { Header } from "../../components/Header";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
+import Link from 'next/link';
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: true,
+        lg: false,
+    })
+
     return (
         <Box>
-            <Header/>
+            <Header />
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-                <Sidebar/>
+                <Sidebar />
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal" >Usuários</Heading>
+                        <Link href="/users/create" passHref>
                         <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="pink"
-                        leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="pink"
+                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
                         >
                             Criar novo
                         </Button>
+                        </Link>
                     </Flex>
-                <Table colorScheme="whiteAlpha">
-                    <Thead>
-                        <Tr>
-                            <Th px="6" color="gray.300" width="8">
-                                <Checkbox colorScheme="pink" />
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
 
-                            </Th>
-                            <Th>
-                                Usuário
-                            </Th>
-                            <Th>Data de Cadastro</Th>
-                            <Th></Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        <Tr>
-                            <Td px="6">
-                                <Checkbox colorScheme="pink"/>
-                            </Td>
-                            <Td>
-                                <Box>
-                                    <Text fontWeight="bold">Beatriz Artimundo</Text>
-                                    <Text fontSize="sm" color="gray.300">beatriz@email.com</Text>
-                                </Box>
-                            </Td>
-                            <Td>
-                                20 de agosto de 2021
-                            </Td>
-                            <Td>
-
-                                <Button
-                                as="a"
-                                size="sm"
-                                fontSize="sm"
-                                colorScheme="pink"
-                                leftIcon={<Icon as={RiPencilLine}/>}
-                                >
-                                Criar novo
-                            </Button>
-                            </Td>
-                        </Tr>
-                    </Tbody>
-                </Table>
-                <Pagination/>
+                                </Th>
+                                <Th>
+                                    Usuário
+                                </Th>
+                                {!isWideVersion && <Th>Data de Cadastro</Th>}
+                                <Th></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Beatriz Artimundo</Text>
+                                        <Text fontSize="sm" color="gray.300">beatriz@email.com</Text>
+                                    </Box>
+                                </Td>
+                                {!isWideVersion &&
+                                    <Td>
+                                        20 de agosto de 2021
+                                    </Td>
+                                }
+                                {!isWideVersion &&
+                                    <Td>
+                                        <Button
+                                            as="a"
+                                            size="sm"
+                                            fontSize="sm"
+                                            colorScheme="pink"
+                                            leftIcon={<Icon as={RiPencilLine} />}
+                                        >
+                                            Editar
+                                        </Button>
+                                    </Td>
+                                }
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                    <Pagination />
                 </Box>
             </Flex>
 
